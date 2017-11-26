451580c61d9b51b038fdb45390bb992ba1cb1bc3
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-26 23:54:18 +0000

    automatically backed up by learn

--- db/migrate/20171126201448_fix_column_name.rb
+++
@@ -1,6 +0,0 @@
-class FixColumnName < ActiveRecord::Migration
-  def change
-    rename_column :recipe_ingredients, :ingredient, :ingredient_id
-    rename_column :recipe_ingredients, :recipe, :recipe_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171126201448) do
+ActiveRecord::Schema.define(version: 20171126191710) do
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
